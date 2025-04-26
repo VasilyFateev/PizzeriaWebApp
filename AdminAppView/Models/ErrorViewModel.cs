2f58d991fc9a5710401692cb3c21fbc82018b2a4
Vasily Fateev <61385487+VasilyFateev@users.noreply.github.com>
2025-04-26 21:10:15 +0300

    Add ASP.net View

---
+++ AdminAppView/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdminAppView.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
